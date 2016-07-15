ac1676797b8fb77e522f21ab4f2aa7bf7d278716
jljsj <liangjun.jiang@alipay.com>
2016-07-15 18:19:30 +0800

    remove tests spec

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,6 @@
 // 设置 body 的高度。。。travis 自动改了document.body.clientHeight的高度;
-document.body.style.height = '500px';
-document.body.style.overflow = 'auto';
-const req = require.context('.', false, /\.spec\.js?$/);
-req.keys().forEach(req);
+// 不搞了，，线上的窗口不行。。。
+// document.body.style.height = '500px';
+// document.body.style.overflow = 'auto';
+// const req = require.context('.', false, /\.spec\.js?$/);
+// req.keys().forEach(req);
