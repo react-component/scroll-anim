6e9db2babeb68239ad52f505b5debfc52a3cbbde
jljsj <liangjun.jiang@alipay.com>
2017-03-23 14:19:29 +0800

    Updates

--- examples/common.js
+++ examples/common.js
@@ -1746,6 +1746,7 @@
 	  onChange: noop,
 	  appear: true
 	};
+	ScrollOverPack.isScrollOverPack = true;
 	
 	exports.default = ScrollOverPack;
 	module.exports = exports['default'];
@@ -6188,14 +6189,14 @@
 	    }
 	
 	    return _ret = (_temp = (_this = (0, _possibleConstructorReturn3.default)(this, _React$Component.call.apply(_React$Component, [this].concat(args))), _this), _this.getParam = function (e) {
-	      _this.clientHeight = _this.target ? _this.target.getBoundingClientRect().height : (0, _util.windowHeight)();
-	      var windowScrollTop = _this.target ? (0, _util.currentScrollTop)() : 0;
+	      _this.clientHeight = _this.target ? _this.target.clientHeight : (0, _util.windowHeight)();
 	      var scrollTop = _this.target ? _this.target.scrollTop : (0, _util.currentScrollTop)();
 	      var domRect = _this.dom.getBoundingClientRect();
-	      var offsetTop = domRect.top + scrollTop + windowScrollTop;
+	      var targetTop = _this.target ? _this.target.getBoundingClientRect().top : 0;
+	      var offsetTop = domRect.top + scrollTop - targetTop;
 	      _this.elementShowHeight = scrollTop - offsetTop + _this.clientHeight;
 	      var playScale = (0, _util.transformArguments)(_this.props.playScale);
-	      var playScaleEnterArray = /([\+\-]?[0-9#\.]+)(px|vh)?/.exec(String(playScale[0]));
+	      var playScaleEnterArray = /([\+\-]?[0-9#\.]+)(px|vh|%)?/.exec(String(playScale[0]));
 	      if (!playScaleEnterArray[2]) {
 	        _this.playHeight = _this.clientHeight * parseFloat(playScale[0]);
 	      } else if (playScaleEnterArray[2] === 'px') {
@@ -6204,7 +6205,7 @@
 	        _this.playHeight = _this.clientHeight * parseFloat(playScaleEnterArray[1]) / 100;
 	      }
 	      var leaveHeight = domRect.height;
-	      var playScaleLeaveArray = /([\+\-]?[0-9#\.]+)(px|vh)?/.exec(String(playScale[1]));
+	      var playScaleLeaveArray = /([\+\-]?[0-9#\.]+)(px|vh|%)?/.exec(String(playScale[1]));
 	      if (!playScaleLeaveArray[2]) {
 	        _this.leavePlayHeight = leaveHeight * parseFloat(playScale[1]);
 	      } else if (playScaleLeaveArray[2] === 'px') {
@@ -6233,13 +6234,14 @@
 	      _Mapped2.default.register(this.props.id, this.dom);
 	    }
 	    var date = Date.now();
+	    this.target = this.props.targetId && document.getElementById(this.props.targetId);
+	
 	    var scrollTop = (0, _util.currentScrollTop)();
 	    if (!scrollTop) {
 	      this.scrollEventListener();
 	    }
 	    var length = _EventDispatcher2.default._listeners.scroll ? _EventDispatcher2.default._listeners.scroll.length : 0;
 	    this.eventType = 'scroll.scrollEvent' + date + length;
-	    this.target = this.props.targetId && document.getElementById(this.props.targetId);
 	    _EventDispatcher2.default.addEventListener(this.eventType, this.scrollEventListener, this.target);
 	  };
 	
@@ -6280,6 +6282,7 @@
 	  onChange: noop,
 	  playScale: 0.5
 	};
+	ScrollElement.isScrollElement = true;
 	exports.default = ScrollElement;
 	module.exports = exports['default'];
 
@@ -23701,7 +23704,7 @@
 	  component: 'div',
 	  always: true
 	};
-	
+	ScrollParallax.isScrollParallax = true;
 	exports.default = ScrollParallax;
 	module.exports = exports['default'];
 
@@ -26840,6 +26843,7 @@
 	  onFocus: noop,
 	  onBlur: noop
 	};
+	ScrollLink.isScrollLink = true;
 	
 	exports.default = ScrollLink;
 	module.exports = exports['default'];
@@ -27076,7 +27080,7 @@
 
 	module.exports = {
 		"name": "rc-scroll-anim",
-		"version": "1.0.2",
+		"version": "1.0.3",
 		"description": "scroll-anim anim component for react",
 		"keywords": [
 			"react",
