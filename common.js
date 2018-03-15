81c9745bbb43e01c769b6620ea640aac3f104c8b
jljsj <liangjun.jiang@alipay.com>
2018-03-15 20:18:08 +0800

    Updates

--- common.js
+++ common.js
@@ -2629,7 +2629,7 @@ __webpack_require__(111)(String, 'String', function (iterated) {
 /* 79 */
 /***/ (function(module, exports) {
 
-module.exports = {"name":"rc-scroll-anim","version":"2.3.0","description":"scroll-anim anim component for react","keywords":["react","react-component","react-scroll-anim","scroll-anim"],"homepage":"https://github.com/react-component/scroll-anim","author":"155259966@qq.com","repository":{"type":"git","url":"https://github.com/react-component/scroll-anim.git"},"bugs":{"url":"https://github.com/react-component/scroll-anim/issues"},"files":["lib","assets/*.css","dist","es"],"licenses":"MIT","main":"./lib/index","module":"./es/index","config":{"port":8020,"entry":{"rc-scroll-anim":["./assets/index.less","./src/index.js"]}},"scripts":{"dist":"rc-tools run dist","build":"rc-tools run build","gh-pages":"rc-tools run gh-pages","start":"rc-tools run server","compile":"rc-tools run compile --babel-runtime","pub":"rc-tools run pub --babel-runtime","lint":"rc-tools run lint","karma":"rc-test run karma","saucelabs":"rc-test run saucelabs","test":"rc-test run test","chrome-test":"rc-test run chrome-test","coverage":"rc-test run coverage"},"devDependencies":{"core-js":"^2.5.1","expect.js":"0.3.x","pre-commit":"1.x","rc-test":"6.x","rc-tools":"6.x","react":"^16.0.0","react-dom":"^16.0.0","rc-animate":"2.x","rc-queue-anim":"^1.3.0"},"pre-commit":["lint"],"dependencies":{"babel-runtime":"6.x","prop-types":"^15.6.0","raf":"3.x","rc-tween-one":"^1.5.0","tween-functions":"1.x"}}
+module.exports = {"name":"rc-scroll-anim","version":"2.3.1","description":"scroll-anim anim component for react","keywords":["react","react-component","react-scroll-anim","scroll-anim"],"homepage":"https://github.com/react-component/scroll-anim","author":"155259966@qq.com","repository":{"type":"git","url":"https://github.com/react-component/scroll-anim.git"},"bugs":{"url":"https://github.com/react-component/scroll-anim/issues"},"files":["lib","assets/*.css","dist","es"],"licenses":"MIT","main":"./lib/index","module":"./es/index","config":{"port":8020,"entry":{"rc-scroll-anim":["./assets/index.less","./src/index.js"]}},"scripts":{"dist":"rc-tools run dist","build":"rc-tools run build","gh-pages":"rc-tools run gh-pages","start":"rc-tools run server","compile":"rc-tools run compile --babel-runtime","pub":"rc-tools run pub --babel-runtime","lint":"rc-tools run lint","karma":"rc-test run karma","saucelabs":"rc-test run saucelabs","test":"rc-test run test","chrome-test":"rc-test run chrome-test","coverage":"rc-test run coverage"},"devDependencies":{"core-js":"^2.5.1","expect.js":"0.3.x","pre-commit":"1.x","rc-test":"6.x","rc-tools":"6.x","react":"^16.0.0","react-dom":"^16.0.0","rc-animate":"2.x","rc-queue-anim":"^1.3.0"},"pre-commit":["lint"],"dependencies":{"babel-runtime":"6.x","prop-types":"^15.6.0","raf":"3.x","rc-tween-one":"^1.5.0","tween-functions":"1.x"}}
 
 /***/ }),
 /* 80 */
@@ -3582,6 +3582,12 @@ var ScrollElement = function (_React$Component) {
         _this.props.onChange({ mode: mode, id: _this.props.id }, e);
       }
       _this.enter = enter;
+    }, _this.startScroll = function () {
+      __WEBPACK_IMPORTED_MODULE_10__EventDispatcher__["a" /* default */].addEventListener(_this.eventType, _this.scrollEventListener, _this.target);
+      var scrollTop = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_11__util__["a" /* currentScrollTop */])();
+      if (!scrollTop) {
+        _this.scrollEventListener();
+      }
     }, _this.scrollEventListener = function (e) {
       _this.getParam(e);
     }, _temp), __WEBPACK_IMPORTED_MODULE_4_babel_runtime_helpers_possibleConstructorReturn___default()(_this, _ret);
@@ -3602,12 +3608,7 @@ var ScrollElement = function (_React$Component) {
 
       var length = __WEBPACK_IMPORTED_MODULE_10__EventDispatcher__["a" /* default */]._listeners.scroll ? __WEBPACK_IMPORTED_MODULE_10__EventDispatcher__["a" /* default */]._listeners.scroll.length : 0;
       this.eventType = 'scroll.scrollEvent' + date + length;
-      __WEBPACK_IMPORTED_MODULE_10__EventDispatcher__["a" /* default */].addEventListener(this.eventType, this.scrollEventListener, this.target);
-
-      var scrollTop = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_11__util__["a" /* currentScrollTop */])();
-      if (!scrollTop) {
-        this.scrollEventListener();
-      }
+      this.startScroll();
     }
   }, {
     key: 'componentWillReceiveProps',
@@ -4593,6 +4594,9 @@ var ScrollOverPack = function (_ScrollElement) {
     value: function componentWillReceiveProps(nextProps) {
       var _this2 = this;
 
+      if (this.props.always !== nextProps.always && nextProps.always) {
+        this.startScroll();
+      }
       this.setState({
         children: __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_10__util__["f" /* toArrayChildren */])(nextProps.children)
       }, function () {
