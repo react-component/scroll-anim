ad5dcda0c5d6365d27de010453ae5fbab4e37dc7
jljsj <liangjun.jiang@alipay.com>
2019-09-05 16:56:29 +0800

    fix test

--- tests/index.js
+++ tests/index.js
@@ -1,7 +1,7 @@
 // 设置 body 的高度。。。travis 自动改了document.body.clientHeight的高度;
 // 不搞了，，线上的窗口不行。。。
-import 'core-js/es6/map';
-import 'core-js/es6/set';
+import 'core-js/es/map';
+import 'core-js/es/set';
 
 const req = require.context('.', false, /\.spec\.js?$/);
 req.keys().forEach(req);
